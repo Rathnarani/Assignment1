5455ff0212a7c33c0d6837a61dcbe73ce975e3f1
Rathnarani <101916723+Rathnarani@users.noreply.github.com>
2022-04-02 03:22:56 -0500

    file is added

---
+++ mul_table.java
@@ -0,0 +1,21 @@
+package Positive_number_multiplication_table;
+
+import java.util.Scanner;
+
+public class mul_table {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stud
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter a positive number:");
+		int n = s.nextInt();
+		while (n < 0) {
+			System.out.println("please enter a positive number");
+			n = s.nextInt();
+		}
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(n + " * " + i + " = " + n * i);
+		}
+
+	}
+
+}
